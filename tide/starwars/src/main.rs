44339951ffc29a5d844fdad96999385a09a3871e
vkill <vkill.net@gmail.com>
2020-04-26 10:45:23 +0800

    upgrade tide to 0.8

--- tide/starwars/src/main.rs
+++ tide/starwars/src/main.rs
@@ -1,22 +1,22 @@
 use async_graphql::http::{playground_source, GQLRequest, GQLResponse};
 use async_graphql::{EmptyMutation, EmptySubscription, IntoQueryBuilder, Schema};
 use starwars::{QueryRoot, StarWars};
-use tide::{http_types, IntoResponse, Request, Response};
+use tide::{Request, Response, StatusCode};
 
 struct ServerState {
     schema: Schema<QueryRoot, EmptyMutation, EmptySubscription>,
 }
 
-async fn graphql_post(mut req: Request<ServerState>) -> Result<Response, http_types::Error> {
+async fn graphql_post(mut req: Request<ServerState>) -> tide::Result<Response> {
     let gql_request: GQLRequest = req
         .body_json()
         .await
-        .map_err(|e| http_types::Error::new(http_types::StatusCode::BadRequest, e))?;
+        .map_err(|e| tide::Error::new(StatusCode::BadRequest, e))?;
 
     let query_builder = gql_request
         .into_query_builder()
         .await
-        .map_err(|e| http_types::Error::new(http_types::StatusCode::BadRequest, e))?;
+        .map_err(|e| tide::Error::new(StatusCode::BadRequest, e))?;
 
     let schema = &req.state().schema;
 
@@ -24,15 +24,17 @@ async fn graphql_post(mut req: Request<ServerState>) -> Result<Response, http_ty
 
     let gql_response = GQLResponse(query_response);
 
-    let resp = Response::new(http_types::StatusCode::Ok).body_json(&gql_response)?;
+    let resp = Response::new(StatusCode::Ok).body_json(&gql_response)?;
 
     Ok(resp)
 }
 
-async fn graphql_playground(_: Request<ServerState>) -> Response {
-    Response::new(http_types::StatusCode::Ok)
+async fn graphql_playground(_: Request<ServerState>) -> tide::Result<Response> {
+    let resp = Response::new(StatusCode::Ok)
         .body_string(playground_source("/", None))
-        .set_header("content-type".parse().unwrap(), "text/html")
+        .set_header("content-type".parse().unwrap(), "text/html");
+
+    Ok(resp)
 }
 
 #[async_std::main]
@@ -46,11 +48,8 @@ async fn main() -> Result<(), std::io::Error> {
     let server_state = ServerState { schema };
     let mut app = tide::with_state(server_state);
 
-    app.at("/").post(|req| async move {
-        graphql_post(req)
-            .await
-            .unwrap_or_else(|e| e.into_response())
-    });
+    app.at("/")
+        .post(|req| async move { graphql_post(req).await });
     app.at("/").get(graphql_playground);
 
     app.listen("0.0.0.0:8000").await?;
