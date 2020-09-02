73c73586c0607a94f1eed35d2fbb2ff065a926c7
Daniel Wiesenberg <daniel@simplificAR.io>
2020-09-02 15:06:20 +0200

    Add rocket Starwars example

---
+++ rocket/starwars/src/main.rs
@@ -0,0 +1,34 @@
+use async_graphql::{EmptyMutation, EmptySubscription, Schema, http::{playground_source, GraphQLPlaygroundConfig}};
+use async_graphql_rocket::{GQLRequest, GraphQL, GQLResponse};
+use rocket::{response::content, routes, State, http::Status};
+use starwars::{QueryRoot, StarWars};
+
+pub type StarWarsSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+
+#[rocket::get("/")]
+fn graphql_playground() -> content::Html<String> {
+    content::Html(playground_source(GraphQLPlaygroundConfig::new("/")))
+}
+
+#[rocket::post("/?<query..>")]
+async fn graphql_query(schema: State<'_, StarWarsSchema>, query: GQLRequest) -> Result<GQLResponse, Status> {
+    query.execute(&schema)
+        .await
+}
+
+#[rocket::post("/", data = "<request>", format = "application/json")]
+async fn graphql_request(schema: State<'_, StarWarsSchema>, request: GQLRequest) -> Result<GQLResponse, Status> {
+    request.execute(&schema)
+        .await
+}
+
+#[rocket::launch]
+fn rocket() -> rocket::Rocket {
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(StarWars::new())
+        .finish();
+
+    rocket::ignite()
+        .attach(GraphQL::fairing(schema))
+        .mount("/", routes![graphql_query, graphql_request, graphql_playground])
+}
