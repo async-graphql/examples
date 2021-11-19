d7fdd5003034de745492ab1ffe88455f7e49c58f
Sunli <scott_s829@163.com>
2021-11-19 08:10:08 +0800

    Add tide examples

--- README.md
+++ README.md
@@ -5,5 +5,7 @@
 - [poem] Examples for `poem`
 - [actix-web] Examples for `actix-web`
 - [warp] Examples for `warp`
-- [federation] Examples for `Federation`
 - [tide] Examples for `tide`
+- [rocket] Examples for `rocket`
+- [axum] Examples for `axum`
+- [federation] Examples for `Federation`
---
+++ tide/starwars/src/main.rs
@@ -0,0 +1,96 @@
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use async_std::task;
+use starwars::{QueryRoot, StarWars};
+use std::env;
+use tide::{http::mime, Body, Response, StatusCode};
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+#[derive(Clone)]
+struct AppState {
+    schema: Schema<QueryRoot, EmptyMutation, EmptySubscription>,
+}
+
+fn main() -> Result<()> {
+    task::block_on(run())
+}
+
+async fn run() -> Result<()> {
+    let listen_addr = env::var("LISTEN_ADDR").unwrap_or_else(|_| "localhost:8000".to_owned());
+
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(StarWars::new())
+        .finish();
+
+    println!("Playground: http://{}", listen_addr);
+
+    let mut app = tide::new();
+
+    app.at("/graphql").post(async_graphql_tide::graphql(schema));
+
+    app.at("/").get(|_| async move {
+        let mut resp = Response::new(StatusCode::Ok);
+        resp.set_body(Body::from_string(playground_source(
+            GraphQLPlaygroundConfig::new("/graphql"),
+        )));
+        resp.set_content_type(mime::HTML);
+        Ok(resp)
+    });
+
+    app.listen(listen_addr).await?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use async_std::prelude::*;
+    use serde_json::json;
+    use std::time::Duration;
+
+    #[test]
+    fn sample() -> Result<()> {
+        task::block_on(async {
+            let listen_addr = find_listen_addr().await;
+            env::set_var("LISTEN_ADDR", format!("{}", listen_addr));
+
+            let server: task::JoinHandle<Result<()>> = task::spawn(async move {
+                run().await?;
+
+                Ok(())
+            });
+
+            let client: task::JoinHandle<Result<()>> = task::spawn(async move {
+                let listen_addr = env::var("LISTEN_ADDR").unwrap();
+
+                task::sleep(Duration::from_millis(300)).await;
+
+                let string = surf::post(format!("http://{}/graphql", listen_addr))
+                    .body(Body::from(r#"{"query":"{ human(id:\"1000\") {name} }"}"#))
+                    .header("Content-Type", "application/json")
+                    .recv_string()
+                    .await?;
+
+                assert_eq!(
+                    string,
+                    json!({"data":{"human":{"name":"Luke Skywalker"}}}).to_string()
+                );
+
+                Ok(())
+            });
+
+            server.race(client).await?;
+
+            Ok(())
+        })
+    }
+
+    async fn find_listen_addr() -> async_std::net::SocketAddr {
+        async_std::net::TcpListener::bind("localhost:0")
+            .await
+            .unwrap()
+            .local_addr()
+            .unwrap()
+    }
+}
