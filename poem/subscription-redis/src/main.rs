c8219078a4b7aa6d84d22e9b79f033088897be4b
Sunli <scott_s829@163.com>
2022-01-06 16:06:06 +0800

    Add poem/subscription-redis example

---
+++ poem/subscription-redis/src/main.rs
@@ -0,0 +1,72 @@
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql::{Context, Object, Result, Schema, Subscription};
+use async_graphql_poem::{GraphQL, GraphQLSubscription};
+use futures_util::{Stream, StreamExt};
+use poem::listener::TcpListener;
+use poem::web::Html;
+use poem::{get, handler, IntoResponse, Route, Server};
+use redis::{AsyncCommands, Client};
+
+struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn version(&self) -> &'static str {
+        std::env!("CARGO_PKG_VERSION")
+    }
+}
+
+struct MutationRoot;
+
+#[Object]
+impl MutationRoot {
+    async fn publish(&self, ctx: &Context<'_>, value: String) -> Result<bool> {
+        let client = ctx.data_unchecked::<Client>();
+        let mut conn = client.get_async_connection().await?;
+        conn.publish("values", value).await?;
+        Ok(true)
+    }
+}
+
+struct SubscriptionRoot;
+
+#[Subscription]
+impl SubscriptionRoot {
+    async fn values(&self, ctx: &Context<'_>) -> Result<impl Stream<Item = String>> {
+        let client = ctx.data_unchecked::<Client>();
+        let mut conn = client.get_async_connection().await?.into_pubsub();
+        conn.subscribe("values").await?;
+        Ok(conn
+            .into_on_message()
+            .filter_map(|msg| async move { msg.get_payload().ok() }))
+    }
+}
+
+#[handler]
+async fn graphql_playground() -> impl IntoResponse {
+    Html(playground_source(
+        GraphQLPlaygroundConfig::new("/").subscription_endpoint("/ws"),
+    ))
+}
+
+#[tokio::main]
+async fn main() {
+    let client = Client::open("redis://127.0.0.1/").unwrap();
+
+    let schema = Schema::build(QueryRoot, MutationRoot, SubscriptionRoot)
+        .data(client)
+        .finish();
+
+    let app = Route::new()
+        .at(
+            "/",
+            get(graphql_playground).post(GraphQL::new(schema.clone())),
+        )
+        .at("/ws", get(GraphQLSubscription::new(schema)));
+
+    println!("Playground: http://localhost:8000");
+    Server::new(TcpListener::bind("0.0.0.0:8000"))
+        .run(app)
+        .await
+        .unwrap();
+}
